88027029ffe5af787b52652dc44bdaa09a0049ee
Rolandi <sd.rolandi@gmail.com>
2021-04-18 20:48:34 -0300

    [feat] - configuração

---
+++ src/routes.tsx
@@ -0,0 +1,22 @@
+import react from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
+import Favorite from './pages/Favorite';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/busca" component={Search} />
+                <Route path="/Watch/:id" component={Watch} />
+                <Route path="/favoritos" component={Favorite} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
