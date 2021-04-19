7fe494e804a889f5353e54145174c894dd165b42
Rolandi <sd.rolandi@gmail.com>
2021-04-19 10:02:09 -0300

    [config] - process.env key youtube

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const requestApi = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key: 'AIzaSyCRJF4eFc3m3ifxTuBXwAzuuThoVpPTJok',
+        key: process.env.REACT_APP_YOUTUBE_KEY
     },
 });
 
