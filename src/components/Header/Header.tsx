6f65187a69056b51c2d3e4602260f363157f0c94
Rolandi <sd.rolandi@gmail.com>
2021-04-19 08:38:43 -0300

    [feat] - componente header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaYoutube } from "react-icons/fa";
+import "react-simple-keyboard/build/css/index.css";
+import './Header.css';
+
+
+export default function Header() {
+
+    return (
+        <>
+            <header className='header'>
+                <div className="header__logo">
+                    <FaYoutube size={36} color="ff0000" />
+                    <h3>My Flix</h3>
+                </div>
+                
+                <div className="header__user">
+                    <img src="https://i.pinimg.com/474x/8e/0c/fa/8e0cfaf58709f7e626973f0b00d033d0.jpg" alt="" />
+                </div>
+            </header>
+        </>
+    );
+}
