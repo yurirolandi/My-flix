0f89b11407d6cb636d10538d138230afb78d6b71
Rolandi <sd.rolandi@gmail.com>
2021-05-01 08:43:18 -0300

    [fix] - try catch em requests

--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -2,57 +2,78 @@ import requestApi from './api';
 
 export const youtubeServices = {
     getChannelVideos: (id: string) => {
-        return requestApi.get('/videos', {
-            params: {
-                part: 'snippet,statistics',
-                id: id,
-            }
-        }).then((response) => {
-            return response.data.items
-        })
+        try {
+            return requestApi.get('/videos', {
+                params: {
+                    part: 'snippet,statistics',
+                    id: id,
+                }
+            }).then((response) => {
+                return response.data.items
+            })
+        } catch (error) {
+            console.log('Ocorreu um erro em getChannelVideos', error);
+
+        }
     },
     getComment: (id: string) => {
-        return requestApi.get('/commentThreads', {
-            params: {
-                part: 'snippet',
-                videoId: id,
-            },
-        }).then((response) => {
-            return response.data.items;
-        })
+        try {
+            return requestApi.get('/commentThreads', {
+                params: {
+                    part: 'snippet',
+                    videoId: id,
+                },
+            }).then((response) => {
+                return response.data.items;
+            })
+        } catch (error) {
+            console.log('Ocorreu um erro em getComment', error);
+        }
     },
     getPopularVideos: () => {
-        return requestApi.get('/videos', {
-            params: {
-                part: 'snippet',
-                chart: 'mostPopular',
-                regionCode: 'BR',
-                maxResults: 20
-            }
-        }).then((response) => {
-            return response.data.items
-        })
+        try {
+            return requestApi.get('/videos', {
+                params: {
+                    part: 'snippet',
+                    chart: 'mostPopular',
+                    regionCode: 'BR',
+                    maxResults: 20
+                }
+            }).then((response) => {
+                return response.data.items
+            })
+        } catch (error) {
+            console.log('Ocorreu um erro em getPopularVideos', error);
+        }
     },
     getSearch: (value: string) => {
-        return requestApi.get('/search', {
-            params: {
-                part: 'snippet',
-                maxResults: 20,
-                q: value,
-                type: 'video',
-            }
-        }).then((response) => {
-            return response.data.items
-        })
+        try {
+            return requestApi.get('/search', {
+                params: {
+                    part: 'snippet',
+                    maxResults: 20,
+                    q: value,
+                    type: 'video',
+                }
+            }).then((response) => {
+                return response.data.items
+            })
+        } catch (error) {
+            console.log('Ocorreu um erro em getSearch', error);
+        }
     },
     getChannels: (id: string) => {
-        return requestApi.get('/channels', {
-            params: {
-                part: 'snippet',
-                id: id
-            }
-        }).then((response) => {
-            return response.data.items[0].snippet.thumbnails.default
-        })
+        try {
+            return requestApi.get('/channels', {
+                params: {
+                    part: 'snippet',
+                    id: id
+                }
+            }).then((response) => {
+                return response.data.items[0].snippet.thumbnails.default
+            })
+        } catch (error) {
+            console.log('Ocorreu um erro em getChannels', error);
+        }
     }
 }
